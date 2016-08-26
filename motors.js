75bc744e7a74d09416cc940a816b4b9dd0a1759d
Rick Waldron <waldron.rick@gmail.com>
2016-08-26 14:56:24 -0400

    motors.js: nitpicking
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- motors.js
+++ motors.js
@@ -1,38 +1,38 @@
-const five = require('johnny-five');
-const Tessel = require('tessel-io');
+"use strict";
+const five = require("johnny-five");
+const Tessel = require("tessel-io");
+const board = new five.Board({
+  io: new Tessel()
+});
 
-const board = new five.Board({ io: new Tessel() });
-
-board.on('ready', () => {
-  // Johnny-Five's `Motors` collection class allows us to control
-  // multiple motors at once.
+board.on("ready", () => {
+  // Johnny-Five's `Motors` collection class allows
+  // us to control multiple motors at once.
   const motors = new five.Motors([
-    { pins: { pwm: 'a5', dir: 'a4', cdir: 'a3' } },     // Left Motor
-    { pins: { pwm: 'b5', dir: 'b4', cdir: 'b3' } },     // Right Motor
+    // Left Motor
+    { pins: { pwm: "a5", dir: "a4", cdir: "a3" } },
+    // Right Motor
+    { pins: { pwm: "b5", dir: "b4", cdir: "b3" } },
   ]);
-  var currentDirection, currentSpeed;
-  function accelerate () {
-    if (currentSpeed <= 255) {
-      currentSpeed += 5;
-      motors.start(currentSpeed);
-      setTimeout(accelerate, 200);
+  let direction = "forward"
+  let speed = 0;
+
+  function accelerate() {
+    if (speed <= 255) {
+      speed += 5;
+      motors[direction](speed);
+      board.wait(200, accelerate);
     } else {
       flipMotorDirection();
     }
   }
-  function flipMotorDirection () {
+
+  function flipMotorDirection() {
     motors.stop();
     board.wait(1000, () => {
-      currentSpeed = 0;
-      if (currentDirection === 'forward') {
-        currentDirection = 'reverse';
-        motors.reverse(currentSpeed); // Set direction of motor, but speed 0
-      } else {
-        currentDirection = 'forward';
-        motors.forward(currentSpeed); // Set direction of motor, but speed 0
-      }
-      console.log(`Running motors in ${currentDirection} direction`);
-      console.log('Accelerating...');
+      speed = 0;
+      direction = direction === "reverse" ? "forward" : "reverse";
+      console.log(`Running motors in ${direction} direction`);
       accelerate();
     });
   }
